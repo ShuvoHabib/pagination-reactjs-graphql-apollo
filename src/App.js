af4962e11118ec2f92f3a3a5a04f32dd3b23616c
shuvohabib <shuvohabib@gmail.com>
2019-03-05 22:11:44 +0600

    Add graphQL pagination

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Query } from "react-apollo";
+import "./App.css";
+import Repos from "./components/Repo";
+import { trendingRepositoriesGQLQuery } from "./data/query";
+import moment from "moment";
 
 class App extends Component {
   render() {
+    const date = new moment(new Date()).subtract(1, "weeks");
+    const formattedDate = date.format("YYYY-MM-DD");
+    const query = `created:>${formattedDate} sort:stars-desc`;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Last Week Trending Repositories</h1>
+        <Query
+          notifyOnNetworkStatusChange={true}
+          query={trendingRepositoriesGQLQuery}
+          variables={{
+            query
+          }}
+        >
+          {({ data, loading, error, fetchMore }) => {
+            if (error) return <p>{error.message}</p>;
+            const search = data.search;
+
+            return (
+              <Repos
+                loading={loading}
+                entries={search}
+                onLoadMore={() =>
+                  fetchMore({
+                    variables: {
+                      query,
+                      cursor: search.pageInfo.endCursor
+                    },
+                    updateQuery: (prevResult, { fetchMoreResult }) => {
+                      const newEdges = fetchMoreResult.search.edges;
+                      const pageInfo = fetchMoreResult.search.pageInfo;
+                      return newEdges.length
+                        ? {
+                            search: {
+                              __typename: prevResult.search.__typename,
+                              edges: [...prevResult.search.edges, ...newEdges],
+                              pageInfo
+                            }
+                          }
+                        : prevResult;
+                    }
+                  })
+                }
+              />
+            );
+          }}
+        </Query>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: "https://api.github.com/graphql",
+  headers: {
+    Authorization: "Bearer 40af13df5456db1ad46ef58cc2857b052241d327"
+  }
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
